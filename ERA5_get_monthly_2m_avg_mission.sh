933ff5d89531590fc616a9e262c167982701499c
Steven Buczkowski <sbuczko1@umbc.edu>
2022-09-16 11:15:44 -0400

    commented out driver file production in script. add as CL option

--- ERA5_get_monthly_2m_avg_mission.sh
+++ ERA5_get_monthly_2m_avg_mission.sh
@@ -3,28 +3,28 @@
 # put ourselves into reception directory
 #cd /asl/models/era5_avg/INCOMING
 
-driver_file_sorted=era5_monthly.driver
-driver_file=${driver_file_sorted}.shuffled
+# driver_file_sorted=era5_monthly.driver
+# driver_file=${driver_file_sorted}.shuffled
 
-# build driver file of year/months to download (already have 2018)
-echo "> Build driver file for download..."
-for month in {08..12}; do
-    echo "2002 $month" >> $driver_file_sorted
-done
+# # build driver file of year/months to download (already have 2018)
+# echo "> Build driver file for download..."
+# for month in {08..12}; do
+#     echo "2002 $month" >> $driver_file_sorted
+# done
 
-for year in {2003..2020}; do
-    for month in {01..12}; do
-	echo "$year $month" >> $driver_file_sorted
-    done
-done
+# for year in {2003..2020}; do
+#     for month in {01..12}; do
+# 	echo "$year $month" >> $driver_file_sorted
+#     done
+# done
 
-for month in {01..07}; do
-    echo "2021 $month" >> $driver_file_sorted
-done
+# for month in {01..07}; do
+#     echo "2021 $month" >> $driver_file_sorted
+# done
 
-# shuffle driver file for interleaved download
-shuf --output=$driver_file $driver_file_sorted
-echo "> Download driver complete. Starting downloads."
+# # shuffle driver file for interleaved download
+# shuf --output=$driver_file $driver_file_sorted
+# echo "> Download driver complete. Starting downloads."
 
 # read through driver file and download year/month combinations
 while IFS= read -r line
@@ -37,6 +37,6 @@ do
     echo ">> Run download for $year/$month..."
     ERA5_get_monthly_2m_avg.py ERA5.ini $year $month >> logfile 2>&1
 
-done < "$driver_file"
+done < "$1"
 
     
